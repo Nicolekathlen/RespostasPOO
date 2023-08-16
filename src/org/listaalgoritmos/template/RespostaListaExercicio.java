77fd5ddc562059d7838883a879c19b0a0427bf67
Nicole Kathlen <nkathlen4@gmail.com>
2023-08-16 03:28:34 -0300

    Exercicios De Poo resolvidos , atividade 1

--- src/org/listaalgoritmos/template/RespostaListaExercicio.java
+++ src/org/listaalgoritmos/template/RespostaListaExercicio.java
@@ -1,144 +1,132 @@
 package org.listaalgoritmos.template;
-
-
-
 public class RespostaListaExercicio {
-	
-	/*
-	 * Utilize apenas o método main para testar a implementação dos métodos. 
-	 * Não se faz necessário implementar dentro do escopo do método.
-	 */
-	public static void main(String[] args) {
-		
+
+    public static void main(String[] args) {
+        calcularImc();
+        calcularAreaTrapezio();
+        maiorEntreDoisInteiros();
+        verificarParImpar();
+        calcularMediaNotas();
+        testarElementosImpares();
+        testarElementosPares();
+        testarMediaAritmetica();
+    }
+
+
+	        /*cálculo do imc */
+    public static void calcularImc() {
+        float peso = 70.0f;
+        float altura = 1.75f;
+        float imc = peso / (altura * altura);
+        System.out.println("IMC: " + imc);
+    }
+
+	        /*cálculo do trapézio */
+
+    public static void calcularAreaTrapezio() {
+        float baseMaior = 10.0f;
+        float baseMenor = 6.0f;
+        float altura = 4.5f;
+        float areaTrapezio = calcularAreaTrapezio(baseMaior, baseMenor, altura);
+        System.out.println("Área do trapézio: " + areaTrapezio);
+    }
+
+	        /* Cálculo de maior entre dois inteiros */ 
+    public static void maiorEntreDoisInteiros() {
+        int a = 10;
+        int b = 7;
+        int maior = maiorEntreDoisInteiros(a, b);
+        System.out.println("Maior entre " + a + " e " + b + " é: " + maior);
+    }
+            /* Cálculo de número ímpar ou par */
+    public static void verificarParImpar() {
+        int numero = 15;
+        boolean par = verificarParImpar(numero);
+        System.out.println(numero + (par ? " é par" : " é ímpar"));
+    }
+           /* Cálculo de média das notas */
+    public static void calcularMediaNotas() {
+        float[] notas = { 7.5f, 8.0f, 9.3f, 6.5f, 8.7f };
+        float media = calcularMediaNotas(notas);
+        System.out.println("Média das notas: " + media);
+    }
+        /* Testando quais elemenentos são ímpar */
+    public static void testarElementosImpares() {
+        int[] array = { 2, 4, 6, 8, 10 };
+        imprimirTesteParImpar(array, "ímpar");
+    }
+ 
+	    /* Testando quais elemenentos são par */
+    public static void testarElementosPares() {
+        int[] array = { 2, 4, 6, 8, 10 };
+        imprimirTesteParImpar(array, "par");
+    }
+                 
+	         /* Testando média aritmética */
+    public static void testarMediaAritmetica() {
+        int[] array = { 2, 4, 6, 8, 10 };
+        float mediaAritmetica = calcularMediaAritmetica(array);
+        System.out.println("Média aritmética dos elementos: " + mediaAritmetica);
+    }
+
+
+
+
+	                    // métodos auxiliares ...
+    public static void imprimirTesteParImpar(int[] array, String tipo) {
+        for (int num : array) {
+            boolean condicao = tipo.equals("par") ? num % 2 == 0 : num % 2 != 0;
+            System.out.println(num + " é " + tipo + "? " + condicao);
+        }
+    }
+
+    public static float calcularAreaTrapezio(float baseMaior, float baseMenor, float altura) {
+        if (baseMaior <= 0 || baseMenor <= 0 || altura <= 0) {
+            throw new IllegalArgumentException("As bases e a altura devem ser maiores que zero.");
+        }
+        return ((baseMaior + baseMenor) * altura) / 2;
+    }
+
+    public static int maiorEntreDoisInteiros(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static boolean verificarParImpar(int numero) {
+        return numero % 2 == 0;
+    }
+
+    public static float calcularMediaNotas(float[] notas) {
+        float soma = 0;
+        for (float nota : notas) {
+            soma += nota;
+        }
+        return soma / notas.length;
+    }
+
+    public static float calcularMediaAritmetica(int[] array) {
+        float soma = 0;
+        for (int num : array) {
+            soma += num;
+        }
+        return soma / array.length;
+    }
+
+	public static void imprimirMaiorMenorElemento(int[] arrayOrig) {
 	}
-	
-	/**
-	 * Método deve calcular o IMC utilizando peso e altura. Como resultado
-	 * o método deve retornar uma variável resultado do tipo float.
-	 * @param peso
-	 * @param altura
-	 * @return resultado
-	 */
-	public static float calcularImc(float peso, float altura) 
-	{
-		return 0f;
-	}
-	
-	/**
-	 * Método deve calcular a área do trapézio independente da unidade de medida.
-	 * Como resultado, o método deve retornar a área calculada.
-	 * @param baseMaior
-	 * @param baseMenor
-	 * @param altura
-	 * @return areaTrapezio
-	 */
-	public static float calcularAreaTrapezio(float baseMaior, float baseMenor, float altura) 
-	{	
-		return 0f;
-	}
-	
-	/**
-	 * O método deve calcular o maior entre dois inteiros, que são fornecidos
-	 * como argumento do método. O retorno deve ser o maior valor dado
-	 * como entrada, sendo do tipo inteiro.
-	 * @param a
-	 * @param b
-	 * @return maiorValor
-	 */
-	public static int maiorEntreDoisInteiros(int a, int b) 
-	{
-		return 0;
-	}
-	
-	/**
-	 * Método verifica se o número apresentado como argumento é ímpar ou par.
-	 * caso seja par, o retorno do método deve ser verdadeiro. Caso contrário,
-	 * o valor retornado deve ser falso.
-	 * @param numero
-	 * @return condicao
-	 */
-	public static boolean verificarParImpar(int numero) 
-	{
-		return false;		
-	}
-	
-	/**
-	 * Método calcula a média aritmetica dos elementos float a partir de um array. 
-	 * O retorno deve ser o resultado do cálculo da média.
-	 * @param notas
-	 * @return media
-	 */
-	public static float calcularMediaNotas(float[] notas) {
-		return 0f;
-	}
-	
-	/**
-	 * Método imprime array de inteiros no sentido inverso, separando os elementos por vírgula.
-	 * O último elemento impresso não deve preceder vírgula. Use o System.out.print() para imprimir os valores.
-	 * Os elementos devem ser impresso numa única linha.
-	 * @param array
-	 */
-	public static void imprimirArrayInverso(int[] array)
-	{
-		System.out.print("");
-	}
-	
-	/**
-	 * Método deve imprimir os elementos primos de um array de inteiros. O formato da impressão 
-	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os elementos e 
-	 * após o último elemento. Utilize System.out.print()
-	 * @param array
-	 */
-	public static void imprimirElementosPrimos(int[] array) 
-	{
-		System.out.print("");
-	}
-	
-	
-	/**
-	 * Método deve imprimir os elementos ímpares de um array de inteiros. O formato da impressão 
-	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os elementos e 
-	 * após o último elemento. Utilize System.out.print()
-	 * @param array
-	 */
-	public static void imprimirElementosImpares(int[] array) 
-	{
-		System.out.print("");
-	}
-	
-	
-	/**
-	 * Método deve imprimir os elementos pares de um array de inteiros. O formato da impressão 
-	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os elementos e 
-	 * após o último elemento. Utilize System.out.print()
-	 * @param array
-	 */
-	public static void imprimirElementosPares(int[] array) 
-	{
-		System.out.print("");
-	}
-	
-	
-	/**
-	 * Método deve imprimir o maior e o menor elemento de um array de inteiros. O formato da impressão 
-	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os elementos e 
-	 * após o último elemento. Utilize System.out.print()
-	 * @param array
-	 */
-	public static void imprimirMaiorMenorElemento(int[] array) 
-	{
-		System.out.print("");
-	}
-	
-	/**
-	 * O método calcula a média aritmética dos elementos de um array de inteiros. 
-	 * Como resultado, o valor da média deve ser retornado no método.
-	 * @param array
-	 * @return media
-	 */
-	public static float calcularMediaAritmetica(int[] array) 
-	{
-		return 0f;
-	}
-	
+
+    public static void imprimirElementosPares(int[] arrayOrig) {
+    }
+
+    public static void imprimirElementosImpares(int[] arrayOrig) {
+    }
+
+    public static void imprimirElementosPrimos(int[] arrayOrig) {
+    }
+
+    public static void imprimirArrayInverso(int[] arrayOrig) {
+    }
+
+    public static float calcularImc(float f, float g) {
+        return 0;
+    }
 }
